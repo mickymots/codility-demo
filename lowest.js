af61f454f8d3a84286a86171edd8d14f3ac69065
Administrator <amit.singh@td.com>
2020-08-14 23:16:00 -0400

    fixed test failure

--- lowest.js
+++ lowest.js
@@ -3,7 +3,7 @@ const lowestFn =  function lowest(A) {
     A.sort((a,b)=> a-b)
     return A.reduce((lowest,current) => {
        if(current == lowest)
-        return lowest + 2
+        return lowest + 1
        else return lowest 
     }, lowest_bound)
     
